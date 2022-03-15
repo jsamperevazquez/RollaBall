92db898ff5618f7279df14c8daaac8c61b6455ff
joseangelsamperevazquez <jsamperevazquez@danielcastelao.org>
2022-03-15 08:37:18 +0100

    Creado el movimiento de la esfera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using  UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    private Rigidbody _rb;
+
+    private float _movementX;
+
+    private float _movementY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        _movementX = movementVector.x;
+        _movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
+        _rb.AddForce(movement * speed);
     }
 }
