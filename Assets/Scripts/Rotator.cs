716022ccd2bb156417d18c74f3e050cd42f49402
joseangelsamperevazquez <jsamperevazquez@danielcastelao.org>
2022-03-18 16:31:05 +0100

    Creado un random para el posicionamiento de los pickUps

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,9 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class Rotator : MonoBehaviour
 {
+    private void Start()
+    {
+        transform.position = new Vector3(Random.Range(-9.0f,9.0f),0.5f,Random.Range(-8.0f,8.0f));
+    }
 
     // Update is called once per frame
     void Update()
